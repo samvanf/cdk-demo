1d8250f8000d089c2864a47d794686efbc12d8a8
Sam VanFossen <samvanf@gmail.com>
2022-11-10 08:09:16 -0500

    Adding ecs structure

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,7 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,15 +9,37 @@ export class CdkDemoStack extends cdk.Stack {
 
     // Creating a new VPC with a public subnet in 2 availability zones.
 
-    new ec2.Vpc(this, 'mainVPC', {
+    const vpc = new ec2.Vpc(this, "mainVPC", {
       maxAzs: 2,
       subnetConfiguration: [
         {
           cidrMask: 24,
-          name: 'public-subnet',
-          subnetType: ec2.SubnetType.PUBLIC
-        }
-      ]
-    })
+          name: "public-subnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
+
+    // Add capacity to it
+    cluster.addCapacity("DefaultAutoScalingGroupCapacity", {
+      instanceType: new ec2.InstanceType("t2.micro"),
+      desiredCapacity: 3,
+    });
+
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, "TaskDef");
+
+    taskDefinition.addContainer("DefaultContainer", {
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      memoryLimitMiB: 512,
+    });
+
+    // Instantiate an Amazon ECS Service
+    const ecsService = new ecs.Ec2Service(this, "Service", {
+      cluster,
+      taskDefinition,
+    });
   }
 }
